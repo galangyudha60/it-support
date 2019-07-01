b3e30913392306496649f4548529638eabbe7d67
galangyudha60 <52424914+galangyudha60@users.noreply.github.com>
2019-07-02 02:23:09 +0700

    Add files via upload

---
+++ assets/js/controller1.js
@@ -0,0 +1,11 @@
+angular.module('harga', []).controller('operasi', function ($scope)
+{
+    $scope.gta = 33;
+    $scope.fc = 31;
+    $scope.apex = 5;
+    $scope.tp = 13;
+    $scope.repeater = 13;
+    $scope.hub = 4;
+    $scope.windows = 100;
+    $scope.mc = 59;
+});
